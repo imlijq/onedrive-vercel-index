87b0d4b1e2dff22f97042b7309c2ae2144d82302
alphabet007 <438728736@qq.com>
2022-08-04 14:27:25 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,13 +8,10 @@ const createFooterMarkup = () => {
 
 const Footer = () => {
   return (
-    {/*
       <div
-        className="w-full border-t border-gray-900/10 p-4 text-center text-xs font-medium text-gray-400 dark:border-gray-500/30"
+        className=""
         dangerouslySetInnerHTML={createFooterMarkup()}
       ></div>    
-    */}
-    <div></div>
   )
 }
 
